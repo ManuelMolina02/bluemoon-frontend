6fd9ef4a6be2c156cfbbb9edd1979dbcd885f0e0
ManuelMolina02 <manuelanmolina@gmail.com>
2021-09-28 19:33:49 -0300

    criando componente sidebar

---
+++ src/components/SideBar.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <a v-b-toggle.sidebar-right >
+
+      <div class="header p-3">
+        <img src="../assets/logo.svg" style="width: 260px" alt="" />
+      </div>
+
+    </a>
+
+    <b-sidebar id="sidebar-right" title="Sidebar" right shadow>
+      <div class="px-3 py-2">
+        <p>
+          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+          consectetur ac, vestibulum at eros.
+        </p>
+        <b-img
+          src="https://picsum.photos/500/500/?image=54"
+          fluid
+          thumbnail
+        ></b-img>
+      </div>
+    </b-sidebar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SideBar',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
