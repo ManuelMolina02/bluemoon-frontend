eb74bd9bae34369c83cc9f2331e85c35ff25df21
ManuelMolina02 <manuelanmolina@gmail.com>
2021-10-02 14:28:16 -0300

    estilizando e refatorando banner

--- src/components/BannerComponent.vue
+++ src/components/BannerComponent.vue
@@ -3,20 +3,19 @@
     <b-card
       overlay
       :img-src="missions.url"
+      img-height="820px"
       text-variant="white"
-      style="border: none !important;"
+      style="border: none !important"
     >
       <b-card-text class="banner-content m-5">
-          <h1 class="w-50" style="margin-top: 80px">{{missions.title}}</h1>
-          <p class="w-50" style="margin-top: 260px">{{missions.explanation}}</p>
+        <h1>{{ missions.title }}</h1>
+        <p>{{ missions.explanation }}</p>
       </b-card-text>
-
     </b-card>
   </div>
 </template>
 
 <script>
-
 export default {
   name: 'BannerComponent',
 
@@ -26,25 +25,34 @@ export default {
         copyright: 'CARA Project',
         date: '2021-09-10',
         explanation:
-          'Discover the cosmos! Each day a different image or photograph of our fascinating universe is featured, along with a brief explanation written by a professional astronomer.',
+          'A cada dia, uma imagem ou fotografia diferente de nosso fascinante universo é apresentada, junto com uma breve explicação escrita por um astrônomo profissional.',
         media_type: 'image',
         service_version: 'v1',
-        title: 'Astronomy Picture of the Day',
+        title: 'Descubra o cosmos! ',
         url: 'https://img.wallpapersafari.com/desktop/1920/1080/76/67/HsdRS0.jpg',
       },
     };
   },
-
 };
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
-.banner-container{
-    max-width: 1580px;
-    border: none !important;
-
-    font-family: Quantico;
+.banner-container {
+  border: none !important;
 }
 
+.banner-content h1 {
+  font-family: "Tourney", cursive;
+  width: 35%;
+
+  font-size: 108px;
+  margin-top: 20px;
+  margin-left: 8rem;
+}
+
+.banner-content p {
+  margin-top: 220px;
+  margin-left: 8rem;
+  width: 35%;
+}
 </style>
