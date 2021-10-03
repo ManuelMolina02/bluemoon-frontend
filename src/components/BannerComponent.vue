4c0e3b317813e2822017a59ba51a6078dfebf50b
ManuelMolina02 <manuelanmolina@gmail.com>
2021-10-03 18:35:03 -0300

    refatorando banner | mudando fontes | enquadramentos de texto

--- src/components/BannerComponent.vue
+++ src/components/BannerComponent.vue
@@ -1,19 +1,33 @@
 <template>
-  <div class="banner-container mx-auto">
+  <div class="banner-container">
+    <div class="logo">
+      <!-- moon orbiting -->
+      <img
+        class="moon-image"
+        src="../assets/frame.svg"
+        style="width: 46px; opacity: 0.91"
+        alt=""
+      />
 
-    <div class="banner-container">
-      <div class="background-image">
-        <img :src="missions.url" />
-      </div>
+      <!-- text logo -->
+      <img
+        class="mx-2"
+        src="../assets/bluemoon.svg"
+        style="width: 232px; opacity: 0.91"
+        alt=""
+      />
+    </div>
 
-      <div class="banner-content">
-        <h1 class="text-uppercase">
-          {{ missions.title }}
-        </h1>
-        <p>
-          {{ missions.explanation }}
-        </p>
-      </div>
+    <div class="background-image">
+      <img :src="missions.url" />
+    </div>
+    <div class="banner-content">
+      <h1 class="text-uppercase">
+        Descubra o<br>cosmos!
+      </h1>
+      <p>
+        {{ missions.explanation }}
+      </p>
     </div>
   </div>
 </template>
@@ -31,7 +45,6 @@ export default {
           'A cada dia, uma imagem ou fotografia diferente de nosso fascinante universo é apresentada, junto com uma breve explicação escrita por um astrônomo profissional.',
         media_type: 'image',
         service_version: 'v1',
-        title: 'Descubra o cosmos! ',
         url: 'https://img.wallpapersafari.com/desktop/1920/1080/76/67/HsdRS0.jpg',
       },
     };
@@ -42,10 +55,16 @@ export default {
 <style scoped>
 /* image banner configs */
 
+.background-image{
+  max-height: 900px;
+}
+
 .background-image img {
   width: 100%;
-  height: 820px;
-  display: block;
+  height: 1060px;
+
+  position: relative;
+  top: -100px;
 
   animation: zoom 25s linear infinite;
 }
@@ -54,41 +73,55 @@ export default {
 
 .banner-content {
   position: absolute;
-  top: 10rem;
+  top: 8rem;
   left: 3rem;
   z-index: 1000;
 }
 
 .banner-content h1 {
+  width: auto;
+  margin-left: 4rem;
+
   font-family: "Audiowide", cursive;
-  color: var(--white);
-  width: 40%;
-  font-size: 118px;
+  font-size: 8rem;
   font-weight: 400;
-  margin-left: 8rem;
+  color: var(--white);
+
   animation: fade-slide-in 4s;
 }
 
 .banner-content p {
-
-  margin-top: 180px;
-  margin-left: 8rem;
-  width: 35%;
-  font-size: 36px;
+  margin-top: 12rem;
+  margin-left: 4rem;
+  width: 40%;
+  font-size: 2.4rem;
   animation: fade-slide-in 4s;
+}
+
+@media screen and (max-width: 1400px) {
+
+  .banner-content {
+  top: 7rem;
+}
+
+  .banner-content p {
+  margin-top: 13rem;
+  width: 55%;
+}
+
 }
 
 @keyframes zoom {
   0% {
-    transform: scale(1);
+    transform: scale(1) rotate(0);
   }
 
   50% {
-    transform: scale(1.3);
+    transform: scale(1.3) rotate(4deg);
   }
 
   100% {
-    transform: scale(1);
+    transform: scale(1) rotate(0);
   }
 }
 
